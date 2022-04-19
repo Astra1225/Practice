9fe36f16c1137422185ef88be9b17dc513b71e43
kim <kim@DESKTOP-QJ5J7UE>
2022-04-19 22:27:35 +0900

    Practice

---
+++ practice/src/co/edu/Hello.java
@@ -0,0 +1,13 @@
+package co.edu;
+
+public class Hello {
+	public static int a = 100;		  //1
+	public static void main(String[] args) {
+		 int b = 10;				//2
+		
+		final double pi =  3.14;		//3
+		System.out.println(a);			//4
+	}
+	
+
+}
